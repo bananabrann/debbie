3b5959758b015c90275740eef5a4cee5ba24f97c
Pierson Brannan <plbrannan95@gmail.com>
2019-02-25 14:18:27 -0500

    WORK: Added test program in debbie.ino for turning on and off pin number 13

--- src/debbie.ino
+++ src/debbie.ino
@@ -1,9 +1,15 @@
-void setup() {
-  // put your setup code here, to run once:
+const int ledPin = 13;
 
+void setup() {
+    Serial.begin(9600);
+    pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+    int delayPeriod = 100;
+    digitalWrite(ledPin, HIGH);
+    delay(delayPeriod);
+    digitalWrite(ledPin, LOW);
+    delay(delayPeriod);
 }
