4c98952edcc6e639aa6e621b8af4ed069c8c55bb
Pierson Brannan <plbrannan95@gmail.com>
2019-03-02 18:45:13 -0500

    WORK, REFACTOR: one contact light, one blinking light experiment successfull. Deleted unused C and C++ files and documents

--- src/debbie.ino
+++ src/debbie.ino
@@ -1,15 +1,21 @@
-const int ledPin = 13;
-
 void setup() {
-    Serial.begin(9600);
-    pinMode(ledPin, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  pinMode(7, OUTPUT);
+  bool setupInProgress = true;
+  while
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-    int delayPeriod = 100;
-    digitalWrite(ledPin, HIGH);
-    delay(delayPeriod);
-    digitalWrite(ledPin, LOW);
-    delay(delayPeriod);
+//  for (int i = 0; i < 5; i += 1) {
+//    digitalWrite(LED_BUILTIN, HIGH);
+//    delay(500);
+//    digitalWrite(LED_BUILTIN, LOW);
+//    delay(500);
+//    printf("For loop");
+//  }
+  digitalWrite(7, HIGH);
+  delay(750);
+  digitalWrite(7, LOW);
+  delay(750);
 }
