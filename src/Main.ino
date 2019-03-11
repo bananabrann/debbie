091abe510a6e6ed7e2c116f8722a7e02a7dac95a
Pierson Brannan <plbrannan95@gmail.com>
2019-03-11 10:32:51 -0400

    WORK: Blinking status light and lcd working (If broken, check wires

---
+++ src/Main.ino
@@ -0,0 +1,22 @@
+// Library for Arduino LCD library:
+#include <LiquidCrystal.h>
+
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() {
+  // Sets up pin 13 for blinking status light.
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+  // Set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  lcd.print("Debbie ON");
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+
+}
