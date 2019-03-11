e9d3211dfa5b0683445c605d6fe4b156d2aeb149
Pierson Brannan <plbrannan95@gmail.com>
2019-03-11 11:44:42 -0400

    WORK: Light turns on quickly when sound is detected

--- src/Main.ino
+++ src/Main.ino
@@ -1,22 +1,29 @@
-// Library for Arduino LCD library:
 #include <LiquidCrystal.h>
 
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
+int soundSensor = 6;
+int soundLED = 13;
+
 void setup() {
-  // Sets up pin 13 for blinking status light.
-  pinMode(LED_BUILTIN, OUTPUT);
-  
-  // Set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
-  lcd.print("Debbie ON");
+  lcd.print("listening...");
+  pinMode (soundSensor, INPUT);
+  pinMode (soundLED, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
-
+  int statusSensor = digitalRead (soundSensor);
+  if (statusSensor == 1)
+  {
+    digitalWrite(soundLED, HIGH);
+  }
+    else 
+    {
+      digitalWrite(soundLED, LOW);
+      delay(100);
+      // Without this delay, the light will turn on and remain on.
+    }
 }
+  
