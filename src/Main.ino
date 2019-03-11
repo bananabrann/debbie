4838a0847de444e0137f9fd0e8acaa552576111a
Pierson Brannan <plbrannan95@gmail.com>
2019-03-11 14:50:10 -0400

    WORK: Added function within gate checker to convert voltage reading into decibels

--- src/Main.ino
+++ src/Main.ino
@@ -3,11 +3,8 @@
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-// int soundSensor = 6;
 int microphoneGate = 6;
-// int soundLED = 13;
 int microphoneLED = 13;
-
 int microphoneEnvelope = A0;
 
 void setup()
@@ -23,32 +20,39 @@ void setup()
 
 void loop()
 {
-    interrupts();
+    //    interrupts();
 
     int statusSensor = digitalRead(microphoneGate);
     int receivingValue = analogRead(microphoneEnvelope);
     int lastReceivedReading;
+    int receivingValueInDb = convertToDb(receivingValue);
 
     if (statusSensor == 1)
     {
         lcd.clear();
         digitalWrite(microphoneLED, HIGH);
 
-        Serial.println(receivingValue);
-
-        lcd.print("Reading is ");
+        lcd.print(receivingValueInDb);
+        lcd.print(" Db, (");
         lcd.print(receivingValue);
+        lcd.print("V)");
 
         delay(1000);
-        //        lcd.clear();
     }
     else
     {
         digitalWrite(microphoneLED, LOW);
 
-        lcd.print("No reading...");
+        lcd.print("Speak louder!");
 
         // There needs to be some sort of very short delay. Do not remove this.
-        delay(0100);
+        delay(0200);
+        lcd.clear();
     }
+}
+
+int convertToDb(int num)
+{
+    int newNum = (20 * (log10(num)));
+    return newNum;
 }
