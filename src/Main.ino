4371729b886fe4dd7e8758ce6ed373ad36fdeef9
Pierson Brannan <plbrannan95@gmail.com>
2019-03-11 13:21:52 -0400

    Work: Sound readings are displaying in an unknown measurement on the lcd

--- src/Main.ino
+++ src/Main.ino
@@ -5,25 +5,47 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 int soundSensor = 6;
 int soundLED = 13;
+int analogPinZero = A0;
 
-void setup() {
-  lcd.begin(16, 2);
-  lcd.print("listening...");
-  pinMode (soundSensor, INPUT);
-  pinMode (soundLED, OUTPUT);
+void setup()
+{
+    Serial.begin(9600);
+
+    lcd.begin(16, 2);
+    lcd.print("listening...");
+
+    pinMode(soundSensor, INPUT);
+    pinMode(soundLED, OUTPUT);
 }
 
-void loop() {
-  int statusSensor = digitalRead (soundSensor);
-  if (statusSensor == 1)
-  {
-    digitalWrite(soundLED, HIGH);
-  }
-    else 
+void loop()
+{
+    int statusSensor = digitalRead(soundSensor);
+    int receivingValue = analogRead(analogPinZero);
+    int lastReceivedReading;
+
+    if (statusSensor == 1)
     {
-      digitalWrite(soundLED, LOW);
-      delay(100);
-      // Without this delay, the light will turn on and remain on.
+        lcd.display();
+        lcd.clear();
+        digitalWrite(soundLED, HIGH);
+
+        Serial.println(receivingValue);
+
+        lcd.print("Reading is ");
+        lcd.print(receivingValue);
+
+        delay(600);
+        lcd.clear();
+    }
+    else
+    {
+        digitalWrite(soundLED, LOW);
+
+        lcd.print("Last reading: ");
+        lcd.print(receivingValue);
+
+        // Without this delay, the light will turn on and remain on.
+        delay(0050);
     }
 }
-  
